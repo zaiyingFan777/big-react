da0edd89189baaafa36696344497ac551472e8d1
zaiying.fan <zaiying.fan@outlook.com>
2025-11-27 19:53:16 +0800

    init

--- packages/react/src/currentBatchConfig.ts
+++
@@ -1,9 +0,0 @@
-interface BatchConfig {
-	transition: number | null;
-}
-
-const ReactCurrentBatchConfig: BatchConfig = {
-	transition: null
-};
-
-export default ReactCurrentBatchConfig;
--- scripts/rollup/dev.config.js
+++
@@ -1,7 +0,0 @@
-import reactDomConfig from './react-dom.config';
-import reactNoopRenderer from './react-noop-renderer.config';
-import reactConfig from './react.config';
-
-export default () => {
-	return [...reactConfig, ...reactDomConfig, ...reactNoopRenderer];
-};
